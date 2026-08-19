59d16cfbffeccfb26a47b7d8bb1acf2bcc645e6e
Limefresh <limefresh@5455@gmail.com>
2026-08-19 15:25:19 +0530

    latest update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://techleads.onrender.com',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
